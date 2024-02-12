3cfda955aa3cdf194df494b6cef51234ba4627e9
Susheel <susheelrai96@gmail.com>
2024-02-12 17:02:55 +0530

    implemeted sound as it was not playing before

--- index.js
+++ index.js
@@ -16,26 +16,26 @@ document.addEventListener("keypress", (event) => {
 
 function makeSound(key){
     switch(key){
-        case "w": var audio = new Audio('/sounds/crash.mp3');
-                  audio.play();
+        case "w": var crash = new Audio('/sounds/crash.mp3');
+                  crash.play();
                   break;
-        case "a": var audio = new Audio('/sounds/kick-bass.mp3');
-                  audio.play();
+        case "a": var kick = new Audio('/sounds/kick-bass.mp3');
+                  kick.play();
                   break;
-        case "s": var audio = new Audio('/sounds/snare.mp3');
-                  audio.play();
+        case "s": var snare = new Audio('/sounds/snare.mp3');
+                  snare.play();
                   break;
-        case "d": var audio = new Audio('/sounds/tom-1.mp3');
-                  audio.play();
+        case "d": var tom_1 = new Audio('/sounds/tom-1.mp3');
+                  tom_1.play();
                   break;
-        case "j": var audio = new Audio('/sounds/tom-2.mp3');
-                  audio.play();
+        case "j": var tom_2 = new Audio('/sounds/tom-2.mp3');
+                  tom_2.play();
                   break;
-        case "k": var audio = new Audio('/sounds/tom-3.mp3');
-                  audio.play();
+        case "k": var tom_3 = new Audio('/sounds/tom-3.mp3');
+                  tom_3.play();
                   break;
-        case "l": var audio = new Audio('/sounds/tom-4.mp3');
-                  audio.play();
+        case "l": var tom_4 = new Audio('/sounds/tom-4.mp3');
+                  tom_4.play();
                   break;
         default: console.log("button not in drum list!@!");
     }
